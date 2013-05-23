41f1940e49d06c33a2b6d11e8404755ccae7587e
Joshua Harlow <harlowja@yahoo-inc.com>
2013-05-22 18:36:35 -0700

    Resync with oslo-incubator.

--- taskflow/openstack/common/importutils.py
+++ taskflow/openstack/common/importutils.py
@@ -3,17 +3,17 @@
 # Copyright 2011 OpenStack Foundation.
 # All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 """
 Import related utilities and helper functions.
@@ -42,10 +42,10 @@ def import_object(import_str, *args, **kwargs):
 
 def import_object_ns(name_space, import_str, *args, **kwargs):
     """
-Import a class and return an instance of it, first by trying
-to find the class in a default namespace, then failing back to
-a full path if not found in the default namespace.
-"""
+    Import a class and return an instance of it, first by trying
+    to find the class in a default namespace, then failing back to
+    a full path if not found in the default namespace.
+    """
     import_value = "%s.%s" % (name_space, import_str)
     try:
         return import_class(import_value)(*args, **kwargs)
@@ -65,4 +65,3 @@ def try_import(import_str, default=None):
         return import_module(import_str)
     except ImportError:
         return default
-
