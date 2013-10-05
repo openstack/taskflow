b1f81badfd2b7b3ef15cc57f7b502e5f40bb06a0
Joshua Harlow <harlowja@gmail.com>
2013-10-05 21:39:57 +0000

    Bump requirements to the latest
    Match the requirements for taskflow with
    the openstack requirements (note the upstream
    requirements are missing 'futures' and 'networkx')
    and update the new hacking violations that were
    detected due to the hacking requirement version
    bump.
    
    Change-Id: I8d1326cf2a8b1ea062f5e9aacd0c4f8261c6531a

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
 setuptools.setup(
