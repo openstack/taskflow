37ec87fdcf54774acf0d55ee0a23a07bfcb5ebee
Ivan A. Melnikov <imelnikov@griddynamics.com>
2014-03-13 12:05:57 +0400

    docs: Relative links improvements
    - replace two separate blocks for previous and next chapter on side
      bar with common 'Navigation' block;
    - add link to table of contents to the block, as it's not that easy
      to find one in current layout.
    
    Change-Id: Iabb191fa94ac9bcb061db8e90d69c87525965527

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,7 @@ extensions = [
 # execute "export SPHINX_DEBUG=1" in your terminal to disable
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = []
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
