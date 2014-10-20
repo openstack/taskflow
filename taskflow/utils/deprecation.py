f3e4bb08631403df83d79a57ea5e71bfc18a2d0e
Joshua Harlow <harlowja@gmail.com>
2014-10-19 19:52:03 -0700

    Use wrapt to provide the deprecated class proxy
    Instead of having our own mini-proxy class use the
    robustness (and correctness) that the wrapt library
    provides to implement a more compliant object proxy
    that handles edge-cases better than our own.
    
    Also adds a few basic sanity tests to ensure that the
    moved/deprecated classes operate as we want them to.
    
    Change-Id: Ib7ca832700583d3ca5e175cb322aa00543cbc475

--- taskflow/utils/deprecation.py
+++ taskflow/utils/deprecation.py
@@ -16,6 +16,8 @@
 
 import warnings
 
+import wrapt
+
 from taskflow.utils import reflection
 
 
@@ -24,72 +26,25 @@ def deprecation(message, stacklevel=2):
     warnings.warn(message, category=DeprecationWarning, stacklevel=stacklevel)
 
 
-# Helper accessors for the moved proxy (since it will not have easy access
-# to its own getattr and setattr functions).
-_setattr = object.__setattr__
-_getattr = object.__getattribute__
-
-
-class MovedClassProxy(object):
-    """Acts as a proxy to a class that was moved to another location.
-
-    Partially based on:
-
-    http://code.activestate.com/recipes/496741-object-proxying/ and other
-    various examination of how to make a good enough proxy for our usage to
-    move the various types we want to move during the deprecation process.
-
-    And partially based on the wrapt object proxy (which we should just use
-    when it becomes available @ http://review.openstack.org/#/c/94754/).
-    """
-
-    __slots__ = [
-        '__wrapped__', '__message__', '__stacklevel__',
-        # Ensure weakrefs can be made,
-        # https://docs.python.org/2/reference/datamodel.html#slots
-        '__weakref__',
-    ]
+class MovedClassProxy(wrapt.ObjectProxy):
+    """Acts as a proxy to a class that was moved to another location."""
 
     def __init__(self, wrapped, message, stacklevel):
-        # We can't assign to these directly, since we are overriding getattr
-        # and setattr and delattr so we have to do this hoop jump to ensure
-        # that we don't invoke those methods (and cause infinite recursion).
-        _setattr(self, '__wrapped__', wrapped)
-        _setattr(self, '__message__', message)
-        _setattr(self, '__stacklevel__', stacklevel)
-        try:
-            _setattr(self, '__qualname__', wrapped.__qualname__)
-        except AttributeError:
-            pass
-
-    def __instancecheck__(self, instance):
-        deprecation(
-            _getattr(self, '__message__'), _getattr(self, '__stacklevel__'))
-        return isinstance(instance, _getattr(self, '__wrapped__'))
-
-    def __subclasscheck__(self, instance):
-        deprecation(
-            _getattr(self, '__message__'), _getattr(self, '__stacklevel__'))
-        return issubclass(instance, _getattr(self, '__wrapped__'))
+        super(MovedClassProxy, self).__init__(wrapped)
+        self._self_message = message
+        self._self_stacklevel = stacklevel
 
     def __call__(self, *args, **kwargs):
-        deprecation(
-            _getattr(self, '__message__'), _getattr(self, '__stacklevel__'))
-        return _getattr(self, '__wrapped__')(*args, **kwargs)
+        deprecation(self._self_message, stacklevel=self._self_stacklevel)
+        return self.__wrapped__(*args, **kwargs)
 
-    def __getattribute__(self, name):
-        return getattr(_getattr(self, '__wrapped__'), name)
+    def __instancecheck__(self, instance):
+        deprecation(self._self_message, stacklevel=self._self_stacklevel)
+        return isinstance(instance, self.__wrapped__)
 
-    def __setattr__(self, name, value):
-        setattr(_getattr(self, '__wrapped__'), name, value)
-
-    def __delattr__(self, name):
-        delattr(_getattr(self, '__wrapped__'), name)
-
-    def __repr__(self):
-        wrapped = _getattr(self, '__wrapped__')
-        return "<%s at 0x%x for %r at 0x%x>" % (
-            type(self).__name__, id(self), wrapped, id(wrapped))
+    def __subclasscheck__(self, instance):
+        deprecation(self._self_message, stacklevel=self._self_stacklevel)
+        return issubclass(instance, self.__wrapped__)
 
 
 def moved_class(new_class, old_class_name, old_module_name, message=None,
