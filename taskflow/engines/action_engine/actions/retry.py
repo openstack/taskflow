426484fe1abf88dc9a1d587b3cc7fe0c688362da
Joshua Harlow <harlowja@yahoo-inc.com>
2015-01-16 12:27:49 -0800

    Mirror the task executor methods in the retry action
    To make understanding the retry action easier have it have
    similar execute and revert functions that the task executor/action
    has so that it's easier to understand the common pattern used for
    execution of the different atom types.
    
    Change-Id: Ia5bb1a6d9684d1add8a213dfb6165c2fea3f9d70

--- taskflow/engines/action_engine/actions/retry.py
+++ taskflow/engines/action_engine/actions/retry.py
@@ -25,6 +25,22 @@ from taskflow.types import futures
 LOG = logging.getLogger(__name__)
 
 
+def _execute_retry(retry, arguments):
+    try:
+        result = retry.execute(**arguments)
+    except Exception:
+        result = failure.Failure()
+    return (ex.EXECUTED, result)
+
+
+def _revert_retry(retry, arguments):
+    try:
+        result = retry.revert(**arguments)
+    except Exception:
+        result = failure.Failure()
+    return (ex.REVERTED, result)
+
+
 class RetryAction(base.Action):
     """An action that handles executing, state changes, ... of retry atoms."""
 
@@ -38,14 +54,14 @@ class RetryAction(base.Action):
 
     def _get_retry_args(self, retry, addons=None):
         scope_walker = self._walker_factory(retry)
-        kwargs = self._storage.fetch_mapped_args(retry.rebind,
-                                                 atom_name=retry.name,
-                                                 scope_walker=scope_walker)
+        arguments = self._storage.fetch_mapped_args(retry.rebind,
+                                                    atom_name=retry.name,
+                                                    scope_walker=scope_walker)
         history = self._storage.get_retry_history(retry.name)
-        kwargs[retry_atom.EXECUTE_REVERT_HISTORY] = history
+        arguments[retry_atom.EXECUTE_REVERT_HISTORY] = history
         if addons:
-            kwargs.update(addons)
-        return kwargs
+            arguments.update(addons)
+        return arguments
 
     def change_state(self, retry, state, result=base.NO_RESULT):
         old_state = self._storage.get_atom_state(retry.name)
@@ -74,13 +90,6 @@ class RetryAction(base.Action):
 
     def execute(self, retry):
 
-        def _execute_retry(kwargs):
-            try:
-                result = retry.execute(**kwargs)
-            except Exception:
-                result = failure.Failure()
-            return (ex.EXECUTED, result)
-
         def _on_done_callback(fut):
             result = fut.result()[-1]
             if isinstance(result, failure.Failure):
@@ -89,20 +98,13 @@ class RetryAction(base.Action):
                 self.change_state(retry, states.SUCCESS, result=result)
 
         self.change_state(retry, states.RUNNING)
-        fut = self._executor.submit(_execute_retry,
+        fut = self._executor.submit(_execute_retry, retry,
                                     self._get_retry_args(retry))
         fut.add_done_callback(_on_done_callback)
         fut.atom = retry
         return fut
 
     def revert(self, retry):
-
-        def _execute_retry(kwargs):
-            try:
-                result = retry.revert(**kwargs)
-            except Exception:
-                result = failure.Failure()
-            return (ex.REVERTED, result)
 
         def _on_done_callback(fut):
             result = fut.result()[-1]
@@ -115,7 +117,7 @@ class RetryAction(base.Action):
         arg_addons = {
             retry_atom.REVERT_FLOW_FAILURES: self._storage.get_failures(),
         }
-        fut = self._executor.submit(_execute_retry,
+        fut = self._executor.submit(_revert_retry, retry,
                                     self._get_retry_args(retry,
                                                          addons=arg_addons))
         fut.add_done_callback(_on_done_callback)
@@ -124,5 +126,5 @@ class RetryAction(base.Action):
 
     def on_failure(self, retry, atom, last_failure):
         self._storage.save_retry_failure(retry.name, atom.name, last_failure)
-        kwargs = self._get_retry_args(retry)
-        return retry.on_failure(**kwargs)
+        arguments = self._get_retry_args(retry)
+        return retry.on_failure(**arguments)
