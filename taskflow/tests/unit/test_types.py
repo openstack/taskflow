fd4772ca84a79c2bea45a6d8571a3644c4055f8a
Joshua Harlow <harlowja@yahoo-inc.com>
2015-03-05 15:54:47 -0800

    Specialize checking for overlaps
    When merging a child graph into it's parents graph we
    can specialize checking for overlaps to avoid some of
    overhead of the default subgraph algorithm (which builds
    a full graph, with edges) since we only care about the
    number of duplicate nodes (not needing a full subgraph
    with retained edges).
    
    Change-Id: Ib211460c58efca3ddb5a254da11aafe44716a639

--- taskflow/tests/unit/test_types.py
+++ taskflow/tests/unit/test_types.py
@@ -78,6 +78,71 @@ class GraphTest(test.TestCase):
         g.freeze()
         self.assertRaises(nx.NetworkXError, g.add_node, "c")
 
+    def test_merge(self):
+        g = graph.DiGraph()
+        g.add_node("a")
+        g.add_node("b")
+
+        g2 = graph.DiGraph()
+        g2.add_node('c')
+
+        g3 = graph.merge_graphs(g, g2)
+        self.assertEqual(3, len(g3))
+
+    def test_merge_edges(self):
+        g = graph.DiGraph()
+        g.add_node("a")
+        g.add_node("b")
+        g.add_edge('a', 'b')
+
+        g2 = graph.DiGraph()
+        g2.add_node('c')
+        g2.add_node('d')
+        g2.add_edge('c', 'd')
+
+        g3 = graph.merge_graphs(g, g2)
+        self.assertEqual(4, len(g3))
+        self.assertTrue(g3.has_edge('c', 'd'))
+        self.assertTrue(g3.has_edge('a', 'b'))
+
+    def test_overlap_detector(self):
+        g = graph.DiGraph()
+        g.add_node("a")
+        g.add_node("b")
+        g.add_edge('a', 'b')
+
+        g2 = graph.DiGraph()
+        g2.add_node('a')
+        g2.add_node('d')
+        g2.add_edge('a', 'd')
+
+        self.assertRaises(ValueError,
+                          graph.merge_graphs, g, g2)
+
+        def occurence_detector(to_graph, from_graph):
+            return sum(1 for node in from_graph.nodes_iter()
+                       if node in to_graph)
+
+        self.assertRaises(ValueError,
+                          graph.merge_graphs, g, g2,
+                          overlap_detector=occurence_detector)
+
+        g3 = graph.merge_graphs(g, g2, allow_overlaps=True)
+        self.assertEqual(3, len(g3))
+        self.assertTrue(g3.has_edge('a', 'b'))
+        self.assertTrue(g3.has_edge('a', 'd'))
+
+    def test_invalid_detector(self):
+        g = graph.DiGraph()
+        g.add_node("a")
+
+        g2 = graph.DiGraph()
+        g2.add_node('c')
+
+        self.assertRaises(ValueError,
+                          graph.merge_graphs, g, g2,
+                          overlap_detector='b')
+
 
 class TreeTest(test.TestCase):
     def _make_species(self):
