d80ee56ea6259265a534231a52146f9fd04c9689
Joshua Harlow <harlowja@yahoo-inc.com>
2015-04-22 17:13:20 -0700

    Use oslo_utils eventletutils to warn about eventlet patching
    Change-Id: I86ba0de51b5c5789efae187ebc1c46ae32ff8b8b

--- taskflow/engines/__init__.py
+++ taskflow/engines/__init__.py
@@ -14,8 +14,16 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo_utils import eventletutils as _eventletutils
 
-# promote helpers to this module namespace
+# Give a nice warning that if eventlet is being used these modules
+# are highly recommended to be patched (or otherwise bad things could
+# happen).
+_eventletutils.warn_eventlet_not_patched(
+    expected_patched_modules=['time', 'thread'])
+
+
+# Promote helpers to this module namespace (for easy access).
 from taskflow.engines.helpers import flow_from_detail   # noqa
 from taskflow.engines.helpers import load               # noqa
 from taskflow.engines.helpers import load_from_detail   # noqa
