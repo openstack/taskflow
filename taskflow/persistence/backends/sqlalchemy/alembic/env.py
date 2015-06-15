64677d277553bac7e6656339872245495d2c371c
Joshua Harlow <harlowja@yahoo-inc.com>
2015-06-15 11:54:05 -0700

    Use alembic upgrade function/command directly
    Instead of implementing a similar version to what the
    upgrade function does, just use it directly instead.
    
    Change-Id: I61a3c9f09c6e0724f2b55951989171ef4aaafe0c

--- taskflow/persistence/backends/sqlalchemy/alembic/env.py
+++ taskflow/persistence/backends/sqlalchemy/alembic/env.py
@@ -60,18 +60,16 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    engine = engine_from_config(config.get_section(config.config_ini_section),
-                                prefix='sqlalchemy.',
-                                poolclass=pool.NullPool)
-
-    connection = engine.connect()
-    context.configure(connection=connection, target_metadata=target_metadata)
-
-    try:
+    connectable = config.attributes.get('connection', None)
+    if connectable is None:
+        connectable = engine_from_config(
+            config.get_section(config.config_ini_section),
+            prefix='sqlalchemy.', poolclass=pool.NullPool)
+    with connectable.connect() as connection:
+        context.configure(connection=connection,
+                          target_metadata=target_metadata)
         with context.begin_transaction():
             context.run_migrations()
-    finally:
-        connection.close()
 
 
 if context.is_offline_mode():
