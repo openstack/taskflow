7bc1be0febf902d404793659bfff7a0109635977
Joshua Harlow <harlowja@yahoo-inc.com>
2015-07-10 17:45:33 -0700

    Unify the zookeeper/redis jobboard iterators
    To make the zookeeper jobboard and redis jobboard iterjobs and wait
    functions that much similar have both return iterator objects from
    the same class iterator.
    
    This makes the iterator code and the jobboard code that much easier
    to follow and understand.
    
    Change-Id: Ia772cde881c2631002140e06684521fd42441534

--- taskflow/jobs/base.py
+++ taskflow/jobs/base.py
@@ -16,11 +16,14 @@
 #    under the License.
 
 import abc
+import collections
 import contextlib
 
 from oslo_utils import uuidutils
 import six
 
+from taskflow import exceptions as excp
+from taskflow import states
 from taskflow.types import notifier
 
 
@@ -145,6 +148,76 @@ class Job(object):
         return "%s: %s (uuid=%s, details=%s)" % (type(self).__name__,
                                                  self.name, self.uuid,
                                                  self.details)
+
+
+class JobBoardIterator(six.Iterator):
+    """Iterator over a jobboard that iterates over potential jobs.
+
+    It provides the following attributes:
+
+    * ``only_unclaimed``: boolean that indicates whether to only iterate
+      over unclaimed jobs
+    * ``ensure_fresh``: boolean that requests that during every fetch of a new
+      set of jobs this will cause the iterator to force the backend to
+      refresh (ensuring that the jobboard has the most recent job listings)
+    * ``board``: the board this iterator was created from
+    """
+
+    _UNCLAIMED_JOB_STATES = (states.UNCLAIMED,)
+    _JOB_STATES = (states.UNCLAIMED, states.COMPLETE, states.CLAIMED)
+
+    def __init__(self, board, logger,
+                 board_fetch_func=None, board_removal_func=None,
+                 only_unclaimed=False, ensure_fresh=False):
+        self._board = board
+        self._logger = logger
+        self._board_removal_func = board_removal_func
+        self._board_fetch_func = board_fetch_func
+        self._fetched = False
+        self._jobs = collections.deque()
+        self.only_unclaimed = only_unclaimed
+        self.ensure_fresh = ensure_fresh
+
+    @property
+    def board(self):
+        """The board this iterator was created from."""
+        return self._board
+
+    def __iter__(self):
+        return self
+
+    def _next_job(self):
+        if self.only_unclaimed:
+            allowed_states = self._UNCLAIMED_JOB_STATES
+        else:
+            allowed_states = self._JOB_STATES
+        job = None
+        while self._jobs and job is None:
+            maybe_job = self._jobs.popleft()
+            try:
+                if maybe_job.state in allowed_states:
+                    job = maybe_job
+            except excp.JobFailure:
+                self._logger.warn("Failed determining the state of"
+                                  " job '%s'", maybe_job, exc_info=True)
+            except excp.NotFound:
+                if self._board_removal_func is not None:
+                    self._board_removal_func(maybe_job)
+        return job
+
+    def __next__(self):
+        if not self._jobs:
+            if not self._fetched:
+                if self._board_fetch_func is not None:
+                    self._jobs.extend(
+                        self._board_fetch_func(
+                            ensure_fresh=self.ensure_fresh))
+                self._fetched = True
+        job = self._next_job()
+        if job is None:
+            raise StopIteration
+        else:
+            return job
 
 
 @six.add_metaclass(abc.ABCMeta)
