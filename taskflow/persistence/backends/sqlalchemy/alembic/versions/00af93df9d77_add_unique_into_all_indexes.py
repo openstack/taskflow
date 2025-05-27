openstack/taskflow

commit 72df15e629883b78d7a922e3b2ea6f78b13b73e4

Sets indexes for all tables in taskflow as unique
Introduces an alembic migration to set all indexes to unique so
upgrades to MySQL 8.4 and onwards do not complain about nonstandard
foreign keys.

Closes-Bug: 2100747

Change-Id: Ie0777edf076cbb4f0fdbdf7b273564b73d935434
Signed-off-by: Jose Castro Leon <jose.castro.leon@cern.ch>

taskflow/persistence/backends/sqlalchemy/alembic/versions/00af93df9d77_add_unique_into_all_indexes.py
@@ -1,0 +1,80 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Add unique into all indexes
+
+Revision ID: 00af93df9d77
+Revises: 40fc8c914bd2
+Create Date: 2025-02-28 15:44:37.066720
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '00af93df9d77'
+down_revision = '40fc8c914bd2'
+
+from alembic import op
+
+
+def upgrade():
+    bind = op.get_bind()
+    engine = bind.engine
+    if engine.name == 'mysql':
+        with op.batch_alter_table("logbooks") as batch_op:
+            batch_op.drop_index("logbook_uuid_idx")
+            batch_op.create_index(
+                index_name="logbook_uuid_idx",
+                table_name="logbooks",
+                columns=['uuid'],
+                unique=True)
+
+        with op.batch_alter_table("flowdetails") as batch_op:
+            batch_op.drop_index("flowdetails_uuid_idx")
+            batch_op.create_index(
+                index_name="flowdetails_uuid_idx",
+                table_name="flowdetails",
+                columns=['uuid'],
+                unique=True)
+
+        with op.batch_alter_table("atomdetails") as batch_op:
+            batch_op.drop_index("taskdetails_uuid_idx")
+            batch_op.create_index(
+                index_name="taskdetails_uuid_idx",
+                table_name="atomdetails",
+                columns=['uuid'],
+                unique=True)
+
+
+def downgrade():
+    bind = op.get_bind()
+    engine = bind.engine
+    if engine.name == 'mysql':
+        with op.batch_alter_table("logbooks") as batch_op:
+            batch_op.drop_index("logbook_uuid_idx")
+            batch_op.create_index(
+                index_name="logbook_uuid_idx",
+                table_name="logbooks",
+                columns=['uuid'])
+
+        with op.batch_alter_table("flowdetails") as batch_op:
+            batch_op.drop_index("flowdetails_uuid_idx")
+            batch_op.create_index(
+                index_name="flowdetails_uuid_idx",
+                table_name="flowdetails",
+                columns=['uuid'])
+
+        with op.batch_alter_table("atomdetails") as batch_op:
+            batch_op.drop_index("taskdetails_uuid_idx")
+            batch_op.create_index(
+                index_name="taskdetails_uuid_idx",
+                table_name="atomdetails",
+                columns=['uuid'])

commit d9f26c51b9a0f2494d0feef91036f843436f3b35

Fix duplicate table_name argument
Fix the following error caused by the regression introduced by
72df15e629883b78d7a922e3b2ea6f78b13b73e4 .

TypeError: CreateIndexOp.__init__() got multiple values for argument
'table_name'

Closes-Bug: #2111806
Change-Id: I3e941ffbb0193056e8e271bf96f6a841a2ba95c0

taskflow/persistence/backends/sqlalchemy/alembic/versions/00af93df9d77_add_unique_into_all_indexes.py
@@ -33,7 +33,6 @@ def upgrade():
             batch_op.drop_index("logbook_uuid_idx")
             batch_op.create_index(
                 index_name="logbook_uuid_idx",
-                table_name="logbooks",
                 columns=['uuid'],
                 unique=True)
 
@@ -41,7 +40,6 @@ def upgrade():
             batch_op.drop_index("flowdetails_uuid_idx")
             batch_op.create_index(
                 index_name="flowdetails_uuid_idx",
-                table_name="flowdetails",
                 columns=['uuid'],
                 unique=True)
 
@@ -49,7 +47,6 @@ def upgrade():
             batch_op.drop_index("taskdetails_uuid_idx")
             batch_op.create_index(
                 index_name="taskdetails_uuid_idx",
-                table_name="atomdetails",
                 columns=['uuid'],
                 unique=True)
 
@@ -62,19 +59,16 @@ def downgrade():
             batch_op.drop_index("logbook_uuid_idx")
             batch_op.create_index(
                 index_name="logbook_uuid_idx",
-                table_name="logbooks",
                 columns=['uuid'])
 
         with op.batch_alter_table("flowdetails") as batch_op:
             batch_op.drop_index("flowdetails_uuid_idx")
             batch_op.create_index(
                 index_name="flowdetails_uuid_idx",
-                table_name="flowdetails",
                 columns=['uuid'])
 
         with op.batch_alter_table("atomdetails") as batch_op:
             batch_op.drop_index("taskdetails_uuid_idx")
             batch_op.create_index(
                 index_name="taskdetails_uuid_idx",
-                table_name="atomdetails",
                 columns=['uuid'])